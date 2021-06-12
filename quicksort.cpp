e5a104453c73d8a374252b213f4d82101492f392
BrunoCavazza <30497134+BrunoCavazza@users.noreply.github.com>
2021-06-12 16:53:12 -0300

    Add files via upload

--- quicksort.cpp
+++ quicksort.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<time.h>
 using namespace std;
 
 void qs(int lista[], int limite_izq, int limite_der){
@@ -22,31 +23,35 @@ void qs(int lista[], int limite_izq, int limite_der){
 	if(limite_izq<der){qs(lista,limite_izq,der);}
 	if(limite_der>izq){qs(lista,izq,limite_der);}
 }
-
-void quicksort(int lista[], int n){
-	qs(lista,0,n-1);
-}
 	
-int main(int argc, const char * argv[]){
-	int lista[]={13,54,6,0,1,-41,-3,17,100,-98};
-	int size = sizeof(lista)/sizeof(int);
-	
-	printf("Esta es la lista desordenada \n");
-
-	for(int i=0; i<size; i++){
-		printf("%d", lista[i]);
-		if(i<size-1)
-			printf(",");
+	void quicksort(int lista[], int n){
+		qs(lista,0,n-1);
 	}
-	
-	printf("\n");
-	quicksort(lista,size);
-	
-	printf("Esta es la misma lista pero ordenada con el metodo Quicksort\n");
-	for(int i=0; i<size; i++){
-		printf("%d", lista[i]);
-		if(i<size-1)
-			printf(",");
-	}
-	return 0;
-}
+		
+		int main(int argc, const char * argv[]){
+			srand(time(NULL));
+			int lista[10], i=0;
+			for(i=0; i<10 ;i++){
+					lista[i] = rand() % 100;
+			}
+			int size = sizeof(lista)/sizeof(int);
+			
+			printf("Esta es la lista desordenada \n");
+			
+			for(int i=0; i<size; i++){
+				printf("%d", lista[i]);
+				if(i<size-1)
+					printf(",");
+			}
+			
+			printf("\n");
+			quicksort(lista,size);
+			
+			printf("Esta es la misma lista pero ordenada con el metodo Quicksort\n");
+			for(int i=0; i<size; i++){
+				printf("%d", lista[i]);
+				if(i<size-1)
+					printf(",");
+			}
+			return 0;
+		}
