214d3a8374d50a3e270bcd3dc4470da69df9f612
Pandulc <85765530+Pandulc@users.noreply.github.com>
2021-06-12 17:07:21 -0300

    Add files via upload

---
+++ Quicksort C++.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+void aleatorios(int []);
+int main(int argc, char *argv[]) {
+	int array[10];
+	int ii=0;
+	aleatorios(array);
+	for(ii=0;ii<10;ii++){
+		printf("%d \t", array[ii]);
+	}
+	return 0;
+}
+void aleatorios(int vector[]){
+	int numero=0;
+	int ii=0;
+	srand(time(NULL));
+	for(ii=0;ii<10;ii++){
+		numero=rand()%100;
+		vector[ii]=numero;
+	}
+}
